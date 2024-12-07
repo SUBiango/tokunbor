3b3cb0911e28cdada05ce80e7716e1ff7ee02de4
Umaru Biango <usbiango@gmail.com>
2024-12-08 01:08:37 +0400

    Added the gTTS client library

---
+++ server/services/ttsService.js
@@ -0,0 +1,37 @@
+const textToSpeech = require('@google-cloud/text-to-speech');
+const fs = require('fs');
+const util = require('util');
+
+// Create a client 
+const client = new textToSpeech.TextToSpeechClient();
+
+/**
+ * Converts text to speech using the Google Cloud Text-to-Speech API.
+ * 
+ * @param {string} text - The text to be converted to speech.
+ * @param {string} [languageCode="en-US"] - The language code for the desired language (default: "en-US").
+ * @param {string} [voiceName="en-US-Wavenet-D"] - The name of the voice to be used for speech synthesis (default: "en-US-Wavenet-D").
+ * @param {number} [speakingRate=1.0] - The speaking rate for the synthesized speech (default: 1.0).
+ * @returns {Promise<Buffer>} - A promise that resolves to the audio content in MP3 format as a Buffer.
+ */
+async function convertTextToSpeech(text, languageCode = "en-US", voiceName = "en-US-Wavenet-D", speakingRate = 1.0) {
+    const request = {
+        input: { text: text },
+        voice: { languageCode, name: voiceName},
+        audioConfig: { audioEncoding: 'MP3', speakingRate }
+    }
+
+    // Perform the text-to-speech request
+    const [response] = await client.synthesizeSpeech(request)
+
+    // Save audio file to disk
+    const writeFile = util.promisify(fs.writeFile)
+    const fileName = 'output.mp3'
+    await writeFile(fileName, response.audioContent, 'binary')
+    console.log(`Audio content written to file ${fileName}`)
+
+    // Return audio content 
+    return response.audioContent
+}
+
+module.exports = { convertTextToSpeech }
