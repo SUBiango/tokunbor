50ef4e5ae19f9c2d9802e5171f934de6a32b135d
Umaru Sahr Biango <usbiango@gmail.com>
2025-04-22 08:01:23 +0400

    Added helmet and rate limiter modules

--- server/index.js
+++ server/index.js
@@ -7,22 +7,34 @@ const User = require('./models/User')
 const History = require('./models/History')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const compression = require('compression')
+const helmet = require('helmet')
 
 // Import Routes
 const userRoutes = require('./routes/userRoutes')
 const historyRoutes = require('./routes/historyRoutes')
 const ttsRoutes = require('./routes/ttsRoutes')
 
-dotenv.config({ path: path.resolve(__dirname, '../.env') })
+dotenv.config({ path: path.resolve(__dirname, '../.env') }) // Load environment variables
 const app = express()
 const PORT = process.env.PORT || 3000
 connectDB()
 
+const RateLimit = require("express-rate-limit")
+const limiter = RateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 20, // limit each IP to 20 requests per windowMs
+    message: "Too many requests from this IP, please try again after 15 minutes"
+})
+
 // Middleware 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
+app.use(cors()) // Enable CORS
+app.use(express.json()) // Parse JSON bodies
+app.use(express.urlencoded({ extended: true })) // Parse URL-encoded bodies
+app.use(cookieParser()) // Parse cookies
+app.use(compression()) // Compress all routes
+app.use(helmet())
+app.use(limiter)
 
 // Route Mount
 app.use("/api/users", userRoutes)
